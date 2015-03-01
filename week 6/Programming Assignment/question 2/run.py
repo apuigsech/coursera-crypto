32e3696f49d246fb06bc8eb7067644e7873057c4
Albert Puigsech Galicia <albert@puigsech.com>
2015-03-01 06:49:26 +0100

    Add solution for exercices (week 6 question 1 and 2)

---
+++ week 6/Programming Assignment/question 2/run.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+# Coursera Crypto I Problem Solution (Week 6, Question 2)
+#
+# Copyright (c) 2015 - Albert Puigsech Galicia (albert@puigsech.com)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+import sys
+import math
+from cryptohelper import *
+
+N = 648455842808071669662824265346772278726343720706976263060439070378797308618081116462714015276061417569195587321840254520655424906719892428844841839353281972988531310511738648965962582821502504990264452100885281673303711142296421027840289307657458645233683357077834689715838646088239640236866252211790085787877
+
+
+def main(argv):
+	print RSA_fact_close_pq(N)[0]
+
+
+if __name__ == "__main__":
+        main(sys.argv)
